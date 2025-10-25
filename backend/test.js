867a6a40f6cfb21cc39aec2532733608820f8379
artemy <artemy.nokhrin@yandex.ru>
2025-10-25 12:40:42 +0500

    add

--- backend/test.js
+++ backend/test.js
@@ -1,33 +1,52 @@
-import SkladService from './src/services/sklad.service.js'
 import Client from './src/utils/got.js';
-import { broadcast } from './src/utils/WebSocket.js';
-import { google } from "googleapis";
 import dotenv from 'dotenv';
 import Details from './src/databases/models/sklad/details.model.js';
 
-const details = await Details.findAll()
-details.map(el => {
-    console.log(el.id)
-})
-const res = await Client.sklad('https://api.moysklad.ru/api/remap/1.2/entity/product', 'post', details.map(el => {
-    return {
+dotenv.config();
+
+const details = await Details.findAll();
+
+// Разбиваем массив на чанки по 900 элементов
+function chunkArray(array, size) {
+  const chunks = [];
+  for (let i = 0; i < array.length; i += size) {
+    chunks.push(array.slice(i, i + size));
+  }
+  return chunks;
+}
+
+const chunks = chunkArray(details, 900);
+const promises = []
+for (const [index, chunk] of chunks.entries()) {
+  console.log(`Отправляем партию ${index + 1} из ${chunks.length}...`);
+
+  const payload = chunk.map(el => ({
+    meta: {
+      href: `https://api.moysklad.ru/api/remap/1.2/entity/product/${el.productId}`,
+      metadataHref: "https://api.moysklad.ru/api/remap/1.2/entity/product/metadata",
+      type: "product",
+      mediaType: "application/json"
+    },
+    attributes: [
+      {
         meta: {
-            "href": `https://api.moysklad.ru/api/remap/1.2/entity/product/${el.productId}`,
-            "metadataHref": "https://api.moysklad.ru/api/remap/1.2/entity/product/metadata",
-            "type": "product",
-            "mediaType": "application/json"
+          href: "https://api.moysklad.ru/api/remap/1.2/entity/product/metadata/attributes/97c9d19a-ae6f-11f0-0a80-082b000da4e9",
+          type: "attributemetadata",
+          mediaType: "application/json"
         },
-        attributes: [{
-            "meta" : {
-                "href" : "https://api.moysklad.ru/api/remap/1.2/entity/product/metadata/attributes/97c9d19a-ae6f-11f0-0a80-082b000da4e9",
-                "type" : "attributemetadata",
-                "mediaType" : "application/json"
-            },
-            value: JSON.stringify({
-                initialData: el.initialData,
-                result: el.result
-            })
-        }]
-    }
-}))
-console.log(res)
+        value: JSON.stringify({
+          initialData: el.initialData,
+          result: el.result
+        })
+      }
+    ]
+  }));
+
+  try {
+    promises.push(Client.sklad('https://api.moysklad.ru/api/remap/1.2/entity/product', 'post', payload ))
+  } catch (err) {
+    console.error(`❌ Ошибка при отправке партии ${index + 1}:`, err.message);
+  }
+}
+const res = await Promise.allSettled(promises)
+console.log(res);

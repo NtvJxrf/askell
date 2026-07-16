b7c9a8cf4f64c616a4a7cb84c22e9f78e50bce30
artemy <artemy.nokhrin@yandex.ru>
2026-07-16 21:18:33 +0500

    add

--- apps/frontend/src/app/widgets/customerorder/WidgetClient.js
+++ apps/frontend/src/app/widgets/customerorder/WidgetClient.js
@@ -3,19 +3,20 @@
 import { useEffect, useRef, useState } from "react";
 import WidgetSDK from "@moysklad/js-widget-sdk";
 
-export default function WidgetClient({ appUid, appId, contextNonce, states, user }) {
+export default function WidgetClient({ appUid, appId, contextNonce, states, user, attributes }) {
     const [initialOrderState, setInitialOrderState] = useState(null);
     const initialOrderStateRef = useRef(null);
 
     useEffect(() => {
         const sdk = WidgetSDK.create();
 
-        const fetchData = async () => {
+        const fetchData = async (objectId) => {
             const response = await fetch(`https://calc.askell.ru/api/backend/proxy/sklad?contextNonce=${contextNonce}`, {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({
                     url: `https://api.moysklad.ru/api/remap/1.2/entity/customerorder/e65e442d-d19c-11f0-0a80-0390000360cd`,
+                    // url: `https://api.moysklad.ru/api/remap/1.2/entity/customerorder/${objectId}`,
                     priority: true
                 })
             });
@@ -30,28 +31,36 @@ export default function WidgetClient({ appUid, appId, contextNonce, states, user
             setInitialOrderState(data);
         };
 
-        sdk.onOpen(async (message) => {
-            await fetchData();
-            sdk.openFeedback(message ? message.messageId : undefined);
+        sdk.onOpen(async ({name, messageId, extensionPoint, objectId, displayMode}) => {
+            await fetchData(objectId);
+            sdk.openFeedback(messageId);
         });
 
         sdk.onChange(({ changeHints, extensionPoint, name, objectState }) => {
             const initial = initialOrderStateRef.current;
-            console.log('objectState', objectState)
-            console.log('initialOrderState', initial)
-            console.log(initial.deliveryPlannedMoment > objectState.deliveryPlannedMoment)
             if (!initial) {
-                console.log('initialOrderState is null, skipping validation');
-                sdk.validationFeedback(true, 'messageText');
+                sdk.validationFeedback(true);
                 return;
             }
-            if (initial.deliveryPlannedMoment > objectState.deliveryPlannedMoment) {
-                console.log('Validation failed: deliveryPlannedMoment is less than initial value');
-                sdk.validationFeedback(false, 'Дата отгрузки меньше изначальной');
+            if (initial.deliveryPlannedMoment > objectState.deliveryPlannedMoment && user.permissions.admin.view != 'ALL') {
+                sdk.validationFeedback(false, `Только администратор может уменьшать дату доставки. Текущая дата: ${initial.deliveryPlannedMoment}, вы пытаетесь установить: ${objectState.deliveryPlannedMoment}`);
                 return;
             }
-            console.log('Validation passed: deliveryPlannedMoment is valid');
-            sdk.validationFeedback(true, 'messageText');
+            if(objectState.state.meta.href == states.customerorder['Поставлено в производство'].meta.href) {
+                const attrs = (objectState.attributes || []).reduce((acc, x) => {
+                    acc[x.name] = x.value
+                    return acc
+                }, {})
+                if (attrs['Вид доставки']?.name !== 'Самовывоз') {
+                    const required = ['Город получателя', 'Вид доставки', 'Телефон получателя', 'Адрес получателя', 'Выбор транспортной компании']
+                    const missing = required.filter(key => !(attrs || {})[key])
+                    if (missing.length > 0) {
+                        sdk.validationFeedback(false, `Вид доставки не "Самовывоз". Заполнены не все обязательные поля для создания ПЗ. Не заполнено: ${missing.join(',')}`);
+                        return
+                    }
+                }
+            }
+            sdk.validationFeedback(true);
         });
 
         return () => {
--- apps/frontend/src/app/widgets/customerorder/page.js
+++ apps/frontend/src/app/widgets/customerorder/page.js
@@ -4,6 +4,7 @@ export default async function WidgetPage({ searchParams }) {
     const { contextKey, appUid, appId } = await searchParams;
     const {user, contextNonce} = await apiFetch(`/users/byContextKey?contextKey=${contextKey}`)
     const states = await apiFetch(`/data-refresher/entity?entity=states&devToken=${process.env.DEV_TOKEN}`)
+    const attributes = await apiFetch(`/data-refresher/entity?entity=attributes&devToken=${process.env.DEV_TOKEN}`)
     return (
         <div className="flex-1 min-h-0 h-full w-full">
             <WidgetClient 
@@ -12,6 +13,7 @@ export default async function WidgetPage({ searchParams }) {
                 contextNonce={contextNonce}
                 states={states}
                 user={user}
+                attributes={attributes}
             />
         </div>
     );
